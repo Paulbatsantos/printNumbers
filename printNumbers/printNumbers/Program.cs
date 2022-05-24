48b882c0eece1f0429168252f66edd12280ba8e3
Paulbatsantos <75324115+Paulbatsantos@users.noreply.github.com>
2022-05-24 15:21:14 -0400

    Add files via upload

---
+++ printNumbers/printNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Support");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Training");
+    }
+    else if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("SupportTraining");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
